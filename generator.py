878a1511da02d03c41d4936733f23dc42459b27c
q10park <parkinkon1@gmail.com>
2020-10-15 22:28:17 +0900

    b

--- generator.py
+++ generator.py
@@ -6,9 +6,17 @@ import torch.optim as optim
 
 from torch.utils.data import Dataset
 
+import os
+from PIL import Image
+import pickle
 
-class NusceneParserQ10(Dataset):  # 커스텀 데이터 셋 만들기
-    def __init__(self):
+
+class NusLoaderQ10(Dataset):  # 커스텀 데이터 셋 만들기
+    def __init__(self, root):
+        self.root = root
+        self.maps = list(sorted(os.listdir(os.path.join(root, "Maps"))))
+        self.ego_poses = list(sorted(os.listdir(os.path.join(root, "Ego_poses"))))
+
         self.x_data = [[73, 80, 75],
                        [93, 88, 93],
                        [89, 91, 90],
@@ -17,9 +25,63 @@ class NusceneParserQ10(Dataset):  # 커스텀 데이터 셋 만들기
         self.y_data = [[152], [185], [180], [196], [142]]
 
     def __len__(self):
-        return len(self.x_data)
+        return len(self.maps)
 
     def __getitem__(self, idx):
+        sample_path = os.path.join(self.root, "Maps", self.maps[idx])
+
+
+
+        map_path = os.path.join(self.root, "Maps", self.maps[idx])
+        ego_pose_path = os.path.join(self.root, "Ego_poses", self.ego_poses[idx])
+        img = Image.open(img_path).convert("RGB")
+
+        boxes = []
+        labels = []
+        tree = ET.parse(boxes_path)
+        root = tree.getroot()
+        num_objs = len(root.findall('object'))
+        for member in root.findall('object'):
+            value = (
+                root.find('folder').text,
+                int(member[4][0].text),
+                int(member[4][1].text),
+                int(member[4][2].text),
+                int(member[4][3].text)
+            )
+            boxes.append(value[1:])
+
+            if value[0] == 'Daisy':
+                label = 0
+            elif value[0] == 'Dandelion':
+                label = 1
+            elif value[0] == 'Sunflower':
+                label = 2
+            elif value[0] == 'Rose':
+                label = 3
+            elif value[0] == 'Tulip':
+                label = 4
+            labels.append(label)
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        image_id = torch.tensor([idx])
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+
+
+
+
         x = torch.FloatTensor(self.x_data[idx])
         y = torch.FloatTensor(self.y_data[idx])
 
